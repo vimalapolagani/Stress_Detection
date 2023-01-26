7c8b08e051ed89361c70c7bfff29311d53dbbdda
vimalapolagani <98327616+vimalapolagani@users.noreply.github.com>
2023-01-26 20:49:11 +0530

    Add files via upload

---
+++ stressdetection.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+df=pd.read_csv('stress.csv')
+df.head()
+
+
+# In[3]:
+
+
+df.info()
+
+
+# In[4]:
+
+
+df.shape
+
+
+# In[5]:
+
+
+df.describe()
+
+
+# In[6]:
+
+
+df.isnull().sum()
+
+
+# In[7]:
+
+
+import nltk
+import re
+from nltk. corpus import stopwords
+import string
+nltk. download( 'stopwords' )
+stemmer = nltk. SnowballStemmer("english")
+stopword=set (stopwords . words ( 'english' ))
+
+def clean(text):
+    text = str(text) . lower()  #returns a string where all characters are lower case. Symbols and Numbers are ignored.
+    text = re. sub('\[.*?\]',' ',text)  #substring and returns a string with replaced values.
+    text = re. sub('https?://\S+/www\. \S+', ' ', text)#whitespace char with pattern
+    text = re. sub('<. *?>+', ' ', text)#special char enclosed in square brackets
+    text = re. sub(' [%s]' % re. escape(string. punctuation), ' ', text)#eliminate punctuation from string
+    text = re. sub(' \n',' ', text)
+    text = re. sub(' \w*\d\w*' ,' ', text)#word character ASCII punctuation
+    text = [word for word in text. split(' ') if word not in stopword]  #removing stopwords
+    text =" ". join(text)
+    text = [stemmer . stem(word) for word in text. split(' ') ]#remove morphological affixes from words
+    text = " ". join(text)
+    return text
+df [ "text"] = df["text"]. apply(clean)
+
+
+# In[8]:
+
+
+pip install wordcloud
+
+
+# In[9]:
+
+
+import matplotlib. pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+text = " ". join(i for i in df. text)
+stopwords = set (STOPWORDS)
+wordcloud = WordCloud( stopwords=stopwords,background_color="white") . generate(text)
+plt. figure(figsize=(10, 10) )
+plt. imshow(wordcloud )
+plt. axis("off")
+plt. show( )
+
+
+# In[10]:
+
+
+from sklearn. feature_extraction. text import CountVectorizer
+from sklearn. model_selection import train_test_split
+
+x = np.array (df["text"])
+y = np.array (df["label"])
+
+cv = CountVectorizer ()
+X = cv. fit_transform(x)
+print(X)
+xtrain, xtest, ytrain, ytest = train_test_split(X, y,test_size=0.33)
+
+
+# In[11]:
+
+
+from sklearn.naive_bayes import BernoulliNB
+model=BernoulliNB()
+model.fit(xtrain,ytrain)
+
+
+# In[13]:
+
+
+user=input("Enter the text")
+data=cv.transform([user]).toarray()
+output=model.predict(data)
+print(output)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
